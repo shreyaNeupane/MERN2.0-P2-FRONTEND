5131aab7f85c9810085559ac5541907913c5bd5c
shreyaNeupane <shreyaneupane49@gmail.com>
2025-07-26 12:42:23 +0545

    conntected rect with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 function App() {
- 
-
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<h1>Hello world</h1>} />
+            <Route path="/register" element={<h1>This is register page</h1>} />
+            <Route path="/login" element={<h1>This is login page</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
+  );
 }
 
-export default App
+export default App;
